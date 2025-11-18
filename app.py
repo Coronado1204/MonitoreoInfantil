9dde3fcdc328c0e20fd4131932f5817370969fd6
JulianCoronado04 <juliandcavanzo@gmail.com>
2025-11-17 23:02:14 -0500

    Integración: añadir embed de Power BI en herramienta (env POWERBI_EMBED_URL) y actualizar README

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -8,10 +9,11 @@ def index():
 
 @app.route('/herramienta', methods=['GET', 'POST'])
 def herramienta():
+    powerbi_url = os.environ.get('POWERBI_EMBED_URL', 'https://app.powerbi.com/view?r=eyJrIjoiNWM4ZDAwYTAtNjJjZi00ZGRkLWJiZGUtMTc5YWI3ZTRiOTJjIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9')
     if request.method == 'POST':
         id_number = request.form.get('id_number', '').strip()
-        return render_template('herramienta.html', id_number=id_number)
-    return render_template('herramienta.html', id_number=None)
+        return render_template('herramienta.html', id_number=id_number, powerbi_url=powerbi_url)
+    return render_template('herramienta.html', id_number=None, powerbi_url=powerbi_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
